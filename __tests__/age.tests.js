f161865d84dfc4c588ad97a7e6800dd8a7fa3636
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-24 08:53:18 -0700

    update BaseAge test 1 to include calculated ages.

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -2,8 +2,15 @@ import BaseAge from "../src/js/age";
 
 describe ('BaseAge', () => {
 
-  test('should create new BaseAge from BaseAge constructor', () => {
+  test('should return calculated ages for other planets', () => {
     let newBaseAge = new BaseAge('Alex', 22);
-    expect(newBaseAge).toEqual({name: 'Alex', earthAge: 22});
+    expect(newBaseAge).toEqual(
+      {earthAge: 22,
+      jupiterAge: 1.854974704890388,
+      marsAge: 11.702127659574469,
+      mercuryAge: 91.66666666666667,
+      name: "Alex",
+      venusAge: 35.483870967741936}
+      );
   })
 })
--- src/js/age.js
+++ src/js/age.js
@@ -1,8 +1,12 @@
-//business logic
+//business logic BaseAge
 
 export default class BaseAge {
   constructor(name, earthAge) {
     this.name = name;
     this.earthAge = earthAge;
+    this.mercuryAge = (this.earthAge / 0.24);
+    this.venusAge = (this.earthAge / 0.62);
+    this.marsAge = (this.earthAge / 1.88);
+    this.jupiterAge = (this.earthAge / 11.86);
   }
 }
