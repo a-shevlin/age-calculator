7643d90ee60003d14aab72abbe2fe46edd12e8d7
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-24 14:17:15 -0700

    update webpack to relate to filename.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Age Calculator',
+      title: 'age-calculator',
       template: './src/index.html',
       inject: 'body'
     })
