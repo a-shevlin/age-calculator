a35e1612bc69d5aa0c288f076e409c8a96fc11b2
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-24 15:45:27 -0700

    add mercury to table rules and update css.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import './css/styles.css';
 import BaseLife from './js/life.js';
 
 $(document).ready(function() {
-  $('form').submit(function(event) {
+  $('form#age').submit(function(event) {
     event.preventDefault();
     const name = $('#nameInput').val();
     const age = parseInt($('#ageInput').val());
     let newUser = new BaseLife(name, age);
+
+    $('.return').slideDown();
+    $('.nameReturn').text(newUser.name);
+
+    $('#earthAge').text(newUser.earthAge);
+    $('#earthLiveExp').text(newUser.earthExpect);
+    $('#earthYrsRem').text(newUser.earthRemain());
+    $('#earthYrsOvr').text(newUser.earthOver());
+
+    $('#earthAge').text(newUser.earthAge);
+    $('#earthLiveExp').text(newUser.earthExpect);
+    $('#earthYrsRem').text(newUser.earthRemain());
+    $('#earthYrsOvr').text(newUser.earthOver());
   });
 });
