ef770d80add0babb46a3efb643968e21bc8b6882
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-27 09:41:10 -0700

    update age.tests.js to have rounded numbers in expect.

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -5,12 +5,14 @@ describe ('BaseAge', () => {
   test('should return calculated ages for other planets', () => {
     let newBaseAge = new BaseAge('Alex', 22);
     expect(newBaseAge).toEqual(
-      {earthAge: 22,
-      jupiterAge: 1.854974704890388,
-      marsAge: 11.702127659574469,
-      mercuryAge: 91.66666666666667,
-      name: "Alex",
-      venusAge: 35.483870967741936}
+      {
+        earthAge: 22,
+        jupiterAge: 1.9,
+        marsAge: 11.7,
+        mercuryAge: 91.7,
+        name: "Alex",
+        venusAge: 35.5
+      }
       );
   })
 })
